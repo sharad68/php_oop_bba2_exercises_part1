413c874beebbfa3f04634e08229e5494286c09b4
sharad68 <sharadbhusal49@gmail.com>
2025-01-15 16:39:16 +0100

    new

--- exercise1.php
+++ exercise1.php
@@ -8,3 +8,91 @@
  * Add the getters and setters for each property.
  * Below the class, instantiate a Game and display its information.
 */
+class Game
+{
+    protected string $name;
+    protected string $editor;
+    protected float $price;
+
+    public function __construct(string $name, string $editor, float $price)
+    {
+
+        $this->setName($name);
+        $this->setEditor($editor);
+        $this->setPrice($price);
+    }
+
+    /**
+     * Get the value of name
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of editor
+     */
+    public function getEditor(): string
+    {
+        return $this->editor;
+    }
+
+    /**
+     * Set the value of editor
+     */
+    public function setEditor(string $editor): self
+    {
+        $this->editor = $editor;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of price
+     */
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set the value of price
+     */
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
+$Game1 = new Game("clash of clans", "Supercell", 100);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Games</h1>
+    <article>
+        <h2><?= $Game1->getName() ?></h2>
+        <h3>Editor: <?= $Game1->getEditor() ?> </h3>
+        <h3>Price : <?= $Game1->getPrice() ?> $</h3>
+</body>
+
+</html>
--- exercise2.php
+++ exercise2.php
@@ -8,3 +8,87 @@
  * Add a method displayInfos() to display the person's information.
  * Create a new object and display its information.
  */
+class Person
+{
+    protected string $first_name;
+    protected string $last_name;
+    protected int $age;
+
+
+    public function __construct(string $first_name, string $last_name, int $age)
+    {
+
+        $this->setFirstName($first_name);
+        $this->setLastName($last_name);
+        $this->setAge($age);
+    }
+
+
+    /**
+     * Get the value of first_name
+     */
+    public function getFirstName(): string
+    {
+        return $this->first_name;
+    }
+
+    /**
+     * Set the value of first_name
+     */
+    public function setFirstName(string $first_name): self
+    {
+        $this->first_name = $first_name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of last_name
+     */
+    public function getLastName(): string
+    {
+        return $this->last_name;
+    }
+
+    /**
+     * Set the value of last_name
+     */
+    public function setLastName(string $last_name): self
+    {
+        $this->last_name = $last_name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of age
+     */
+    public function getAge(): int
+    {
+        return $this->age;
+    }
+
+    /**
+     * Set the value of age
+     */
+    public function setAge(int $age): self
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+    public function displayInfos()
+    {
+        return "
+        First Name:    {$this->first_name}
+        Last Name:      {$this->last_name}
+        Age:    {$this->age}
+        ";
+    }
+}
+
+$person1 = new Person("sharad", "bhusal", 21);
+$person2 = new Person("shrine", "sigdel", 20);
+
+echo $person1->displayInfos();
+echo $person1->displayInfos();
